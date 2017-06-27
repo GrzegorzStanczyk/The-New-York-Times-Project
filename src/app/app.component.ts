b7f1449140e6afb1ab859f03681c91c7ea07a347
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2017-06-27 23:49:00 +0200

    Added window scroll event using @HostListener with thottleTime for more articles request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
 import { Response } from '@angular/http'
 import { Observable } from 'rxjs';
+import { DOCUMENT } from '@angular/platform-browser';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+
 
 import { ServerRequestService } from './services/server-request.service';
 import { IMyDrpOptions } from 'mydaterangepicker';
@@ -12,17 +16,20 @@ import { IMyDrpOptions } from 'mydaterangepicker';
 })
 
 export class AppComponent {
-  title: string = 'The New York Times Project';
-  inputSearch: string = '';
-  renderContent: Array<any> = []
-  currentPage: number = 0;
+  public title: string = 'The New York Times Project';
+  public renderContent: Array<any> = []
+  public scrollEvent: Subject<MouseEvent> = new Subject<MouseEvent>();
+  public scrollObservable = this.scrollEvent.asObservable().throttleTime(200);
+  
+  public inputSearch: string = '';
+  public currentPage: number = 0;
+  public model: Object;
 
-  myDateRangePickerOptions: IMyDrpOptions = {
+  public myDateRangePickerOptions: IMyDrpOptions = {
     dateFormat: 'yyyy.mm.dd',
   };
-  
-  model;
-  
+
+
   // For example initialize to specific date (09.10.2018 - 19.10.2018). It is also possible
   // to set initial date range value using the selDateRange attribute.
   // private model: Object = {
@@ -30,30 +37,38 @@ export class AppComponent {
   //   endDate: { year: 2018, month: 10, day: 19 }
   // };
 
-  constructor(private serverRequest: ServerRequestService) {
+  constructor(private serverRequest: ServerRequestService, @Inject(DOCUMENT) private document: Document) {
   }
 
-  onSearch(): void {
+  public onSearch(): void {
     this.serverRequest
       .getServers(this.inputSearch || "Poland", this.currentPage, this.model)
       .subscribe(data => {
         this.renderContent = data.response.docs;
       },
-      (error) => console.log(error))
+      (error) => console.log(error));
   }
 
-  onGetMore(e): void {
-    e.preventDefault();
+  public onGetMore(): void {
     this.currentPage += 1;
     this.serverRequest
       .getServers(this.inputSearch || "Poland", this.currentPage, this.model)
       .subscribe(data => {
         this.renderContent = [...this.renderContent, ...data.response.docs];
       },
-      (error) => console.log(error))
+      (error) => console.log(error));
+  }
+
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event): void {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.scrollEvent.next(event);
+    }
   }
 
   ngOnInit(): void {
-    this.onSearch()
+    this.onSearch();
+    this.scrollObservable.subscribe(() => this.onGetMore());
   }
 }
